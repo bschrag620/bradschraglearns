8be7a0cf3107aed89740b7ac2e237b90a0d3533f
Brad Schrag <brad.schrag@gmail.com>
2019-08-25 20:03:39 -0500

    adding models

--- db/migrate/20190824161149_create_users.rb
+++ db/migrate/20190824161149_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :password_digest
       t.text :bio
+      t.integer :role_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_161149) do
+ActiveRecord::Schema.define(version: 2019_08_26_003530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admin_users", force: :cascade do |t|
+  create_table "roles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
