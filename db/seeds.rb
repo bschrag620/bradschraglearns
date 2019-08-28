ed750279094ff70c52c580bb9e6444a9b0feb464
Brad Schrag <brad.schrag@gmail.com>
2019-08-28 11:52:40 -0500

    added seeds for roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Role.create(:description => 'admin')
+author = Role.create(:description => 'author')
+anon = Role.create(:description => 'anonymous')
